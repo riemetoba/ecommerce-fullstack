b5afd35bdc2c9392a7750f2fbb83cbd6043c90f5
Toba <riemehasan06@gmail.com>
2026-09-07 22:22:54 +0600

    express setup

---
+++ index.js
@@ -0,0 +1,12 @@
+require("dotenv").config()
+const express = require("express")
+const app = express()
+
+app.use(express.json())
+
+
+const port = process.env.PORT || 5000
+app.listen(port, ()=>{
+    console.log(`Server is running on port: ${port}`);
+    
+})
