8b37bfd8f56029e11568894644ba0fbc4ca0fcd0
Toba <riemehasan06@gmail.com>
2026-09-08 20:40:06 +0600

    authRoute created

--- index.js
+++ index.js
@@ -5,6 +5,9 @@ const app = express()
 app.use(express.json())
 
 
+app.use('/api/v1/auth')
+
+
 const port = process.env.PORT || 5000
 app.listen(port, ()=>{
     console.log(`Server is running on port: ${port}`);
