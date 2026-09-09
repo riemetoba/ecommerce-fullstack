7476803ed39cbba4e6d23fac27dd1c6b3385a825
Toba <riemehasan06@gmail.com>
2026-09-09 21:48:35 +0600

    all fields validation done

--- routes/authRouter.js
+++ routes/authRouter.js
@@ -1,17 +1,15 @@
-const express = require('express')
-const _ = express.Router()
+const express = require("express");
+const _ = express.Router();
 
-
-
-
-
-
-
-
-
-
-
-
-
+_.post("/registration", (req, res) => {
+  let { fullName, email, password, confirmPassword, terms } = req.body;
+  if (!fullName || !email || !password || !confirmPassword || !terms) {
+    return res.status(400).json({
+      success: false,
+      message: "Please fill all the fields",
+    });
+  }
+  // res.json('regitration done')
+});
 
 module.exports = _;
