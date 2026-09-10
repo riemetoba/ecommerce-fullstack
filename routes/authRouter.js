e0002d5a44293b20493a93b8d9b831211fe20593
Toba <riemehasan06@gmail.com>
2026-09-10 21:02:37 +0600

    email validation done

--- routes/authRouter.js
+++ routes/authRouter.js
@@ -1,15 +1,45 @@
 const express = require("express");
 const _ = express.Router();
+const emailRegex = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
+const passwordRegex = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/;
+
+
 
 _.post("/registration", (req, res) => {
   let { fullName, email, password, confirmPassword, terms } = req.body;
+
+  //  fields validation
   if (!fullName || !email || !password || !confirmPassword || !terms) {
     return res.status(400).json({
       success: false,
       message: "Please fill all the fields",
     });
   }
-  // res.json('regitration done')
+
+  // email validation
+  if (!emailRegex.test(email)) {
+     return res.status(400).json({
+      success: false,
+      message: "Please enter a valid Email",
+    });
+  }
+  // password validation
+  if (!emailRegex.test(email)) {
+     return res.status(400).json({
+      success: false,
+      message: "Please enter a valid Email",
+    });
+  }
+
+  //   password, confirmPassword validation
+  if (password !== confirmPassword) {
+    return res.status(400).json({
+      success: false,
+      message: "Password do not match",
+    });
+  }
+
+  
 });
 
 module.exports = _;
