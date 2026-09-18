9b70229f8fccbd1e42a3aa4f242afa7bac8fbc74
Toba <riemehasan06@gmail.com>
2026-09-19 01:56:45 +0600

    try catch added in userController, adminController

--- controllers/adminController.js
+++ controllers/adminController.js
@@ -1,81 +1,114 @@
 const User = require('../models/userSchema')
 
+let allUserController = async (req, res) => {
+    try {
+        let users = await User.find({}).select('-password')
 
-let allUserController = async (req, res)=>{
-
-   let users = await User.find({}).select('-password')
-
-    res.status(200).json({
-        success: true,
-        message: `${users.length} user found`,
-        data: users
-    })
-    
-}
-// ==================================
-
-let deleteUserController = async (req, res) => {
-    let {id} = req.params
-
-    let deletedUser = await User.findByIdAndDelete(id)
-
-    if(!deletedUser){
-        return res.status(404).json({
+        res.status(200).json({
+            success: true,
+            message: `${users.length} user found`,
+            data: users
+        })
+    } catch (error) {
+        res.status(500).json({
             success: false,
-            message: "user not found"
+            message: error.message
         })
     }
-
-    res.status(200).json({
-        success: true,
-        message: "user deleted successfully"
-    })
-}
-// ====================================
-
-let singleUserController = async(req, res)=>{
-    let {id} = req.params
-    let data = await User.findOne({_id: id}).select('-password')
-
-    res.status(200).json({
-        success: true,
-        message: `User info`,
-        data: data
-    })
-}
-// ==============================
-
-let activeUserController = async(req, res)=>{
-    let data = await User.find({status: 'active'})
-
-    res.status(200).json({
-        success: true,
-        message: `Active User info`,
-        data: data
-    })
-}
-// ===================================
-
-let deactiveUserController = async(req, res)=>{
-    let data = await User.find({status: 'deactive'})
-
-    res.status(200).json({
-        success: true,
-        message: `Deactive User info`,
-        data: data
-    })
-}
-// ==========================================
-
-let updateUserController = async(req, res)=>{
-    let {id} = req.params
-    await User.findByIdAndUpdate({_id: id}, req.body, {new: true})
-
-    res.status(200).json({
-        success: true,
-        message: `User Updated`
-    })
 }
 
+let deleteUserController = async (req, res) => {
+    try {
+        let {id} = req.params
+
+        let deletedUser = await User.findByIdAndDelete(id)
+
+        if(!deletedUser){
+            return res.status(404).json({
+                success: false,
+                message: "user not found"
+            })
+        }
+
+        res.status(200).json({
+            success: true,
+            message: "user deleted successfully"
+        })
+    } catch (error) {
+        res.status(500).json({
+            success: false,
+            message: error.message
+        })
+    }
+}
+
+let singleUserController = async(req, res) => {
+    try {
+        let {id} = req.params
+        let data = await User.findOne({_id: id}).select('-password')
+
+        res.status(200).json({
+            success: true,
+            message: `User info`,
+            data: data
+        })
+    } catch (error) {
+        res.status(500).json({
+            success: false,
+            message: error.message
+        })
+    }
+}
+
+let activeUserController = async(req, res) => {
+    try {
+        let data = await User.find({status: 'active'})
+
+        res.status(200).json({
+            success: true,
+            message: `Active User info`,
+            data: data
+        })
+    } catch (error) {
+        res.status(500).json({
+            success: false,
+            message: error.message
+        })
+    }
+}
+
+let deactiveUserController = async(req, res) => {
+    try {
+        let data = await User.find({status: 'deactive'})
+
+        res.status(200).json({
+            success: true,
+            message: `Deactive User info`,
+            data: data
+        })
+    } catch (error) {
+        res.status(500).json({
+            success: false,
+            message: error.message
+        })
+    }
+}
+
+let updateUserController = async(req, res) => {
+    try {
+        let {id} = req.params
+        await User.findByIdAndUpdate({_id: id}, req.body, {new: true})
+
+        res.status(200).json({
+            success: true,
+            message: `User Updated`
+        })
+    } catch (error) {
+        res.status(500).json({
+            success: false,
+            message: error.message
+        })
+    }
+}
 
 module.exports = {allUserController, deleteUserController, singleUserController, activeUserController, deactiveUserController, updateUserController}
--- controllers/userController.js
+++ controllers/userController.js
@@ -1,56 +1,78 @@
 const User = require('../models/userSchema')
 const Category = require('../models/categorySchema')
 
-let userController = (req, res)=>{
-    res.send("Hello user");
-    
-}
-// =======================
-let updateController = async(req, res)=>{
-    let {id} = req.params
-    await User.findByIdAndUpdate({_id: id}, req.body, {new: true})
-
-    res.status(200).json({
-        success: true,
-        message: `User Updated`
-    })
-}
-// ========================================
-
-let createCategoryController = async(req, res)=>{
-    let {name} = req.body
-    let existingName = await Category.findOne({name: name.toLowerCase()})
-
-    if (existingName) {
-        return res.status(400).json({
-        success: false,
-        message: "Category already Exist"
-    })
+let userController = (req, res) => {
+    try {
+        res.send("Hello user");
+    } catch (error) {
+        res.status(500).json({
+            success: false,
+            message: error.message
+        })
     }
-
-    let category = new Category({
-        name: name.toLowerCase()
-    })
-    await category.save()
-
-    res.status(201).json({
-        success: true,
-        message: "Category created"
-    })
-}
-// ========================================
-
-let getAllCategoryController = async(req, res)=>{
-    let allCategory = await Category.find({})
-
-     res.status(200).json({
-        success: true,
-        message: "All Category",
-        data: allCategory
-    })
-  
 }
 
+let updateController = async (req, res) => {
+    try {
+        let {id} = req.params
+        await User.findByIdAndUpdate({_id: id}, req.body, {new: true})
 
+        res.status(200).json({
+            success: true,
+            message: `User Updated`
+        })
+    } catch (error) {
+        res.status(500).json({
+            success: false,
+            message: error.message
+        })
+    }
+}
+
+let createCategoryController = async (req, res) => {
+    try {
+        let {name} = req.body
+        let existingName = await Category.findOne({name: name.toLowerCase()})
+
+        if (existingName) {
+            return res.status(400).json({
+                success: false,
+                message: "Category already Exist"
+            })
+        }
+
+        let category = new Category({
+            name: name.toLowerCase()
+        })
+        await category.save()
+
+        res.status(201).json({
+            success: true,
+            message: "Category created"
+        })
+    } catch (error) {
+        res.status(500).json({
+            success: false,
+            message: error.message
+        })
+    }
+}
+
+let getAllCategoryController = async (req, res) => {
+    try {
+        let allCategory = await Category.find({})
+
+        res.status(200).json({
+            success: true,
+            message: "All Category",
+            data: allCategory
+        })
+    } catch (error) {
+        res.status(500).json({
+            success: false,
+            message: error.message
+        })
+    }
+}
 
 module.exports = {userController, updateController, createCategoryController, getAllCategoryController}
