dbacaab73e5744f600e9ef0aaf406b536d9e3aee
Toba <riemehasan06@gmail.com>
2026-09-18 11:46:51 +0600

    feat: allUserController

--- controllers/adminController.js
+++ controllers/adminController.js
@@ -1,8 +1,18 @@
-let adminController = (req, res)=>{
-    res.send("Hello admin");
+const User = require('../models/userSchema')
+
+
+let allUserController = async (req, res)=>{
+
+   let users = await User.find({})
+
+    res.status(200).json({
+        success: true,
+        message: `${users.length} user found`,
+        data: users
+    })
     
 }
 
 
 
-module.exports = {adminController}
+module.exports = {allUserController}
--- routes/adminRouter.js
+++ routes/adminRouter.js
@@ -1,11 +1,11 @@
 const express = require("express");
-const { adminController } = require("../controllers/adminController");
+const { allUserController } = require("../controllers/adminController");
 const _ = express.Router();
 
 
 
 
 
-_.post("/delete/vendor", adminController)
+_.post("/all-user", allUserController)
 
 module.exports = _;
